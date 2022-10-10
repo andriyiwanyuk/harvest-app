de771d4a3e28a057e804c0e7fb4d2c4bbff4c4a5
Andriy Ivanyuk <aivanyuk@Neonomics.io>
2022-10-10 21:49:46 +0300

    module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'services', component: ServicesComponent }
+  ];
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ServicesComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class PagesModule { }
