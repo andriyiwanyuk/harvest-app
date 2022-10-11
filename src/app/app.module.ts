1b2ab7cc1f76399cbb47496fa72da7bbfcdd463e
Andriy Ivanyuk <aivanyuk@Neonomics.io>
2022-10-11 17:10:53 +0300

    hero added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeroComponent
   ],
   imports: [
     BrowserModule,
